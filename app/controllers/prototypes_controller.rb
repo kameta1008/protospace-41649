c6e705665851f09d33529f5b2a87f5098de6fcf9
kameta1008 <takemarue202@gmail.com>
2024-10-25 17:06:07 +0900

    リダイレクト処理

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only:[:new, :edit, :destroy]
   before_action :set_prototype, only: [:edit, :show, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -53,7 +53,7 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @prototype.user_id
+    unless user_signed_in? && current_user == @prototype.user
       redirect_to action: :index
     end
   end
